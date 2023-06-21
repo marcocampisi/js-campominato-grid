fae627a903c5c604edab5c2bedf77151c1e00921
marcocampisi <marco.campisi@outlook.it>
2023-06-21 15:27:06 +0200

    Add difficulty check

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 const gridContainer = document.querySelector('#grid-container');
 
-for (let i = 1; i <= 100; i++) {
-    const mySquare = document.createElement('div');
-    mySquare.classList.add('myCell')
-    mySquare.append(i);
-    mySquare.addEventListener('click', function() {
-        this.classList.toggle('active');
-        console.log(this.innerHTML);
-    });
+function selectDifficulty(difficulty) {
+    let gridSize;
 
-    gridContainer.append(mySquare);
+    switch (difficulty) {
+        case 'facile':
+            gridSize = 100;
+            break;
+        case 'normale':
+            gridSize = 81;
+            break;
+        case 'difficile':
+            gridSize = 49;
+            break;
+        default:
+            console.log('Difficoltà non valida');
+            return;
+    }
 
+    for (let i = 1; i <= gridSize; i++) {
+        const mySquare = document.createElement('div');
+        mySquare.classList.add('myCell')
+        mySquare.append(i);
+        mySquare.addEventListener('click', function() {
+            this.classList.toggle('active');
+            console.log(this.innerHTML);
+        });
+    
+        gridContainer.append(mySquare);
+    
+    }
 }
